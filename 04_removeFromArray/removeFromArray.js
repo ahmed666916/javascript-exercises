1ba0aade7c4cb909cca4bc27d4899d9bf0c1399d
ahmed666916 <ahmed666916@gmail.com>
2024-01-22 22:29:08 +0500

    exercise 04 in progress

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, arg) {
+    let newArray = [];
 
+    for (let i = 0; i < array.length; i++) {
+       
+        
+        if (array[i] !== arg) {
+            newArray.push(array[i]);
+        }
+    
+    }
+    return newArray
 };
 
 // Do not edit below this line
