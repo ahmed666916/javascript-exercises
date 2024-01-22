ca4b55b5ec00c624f92cee1b316996317881decf
ahmed666916 <ahmed666916@gmail.com>
2024-01-22 21:56:28 +0500

    done exercise 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let finalArray = [];
+    let result="";
+    for (let i = 0; i < string.length; i++) {
+        finalArray = finalArray.concat(string[i]);
+    }
+    let loop = finalArray.length;
+    for (let j = 0; j < loop; j++) {
+        result = result + finalArray.pop();
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
