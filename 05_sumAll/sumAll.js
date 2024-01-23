81e5a020c49b12ebbccd7c24bc5331e2fbb41d98
ahmed666916 <ahmed666916@gmail.com>
2024-01-23 20:28:47 +0500

    05 exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
 const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return "ERROR";
+        
+    } else if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+
+    if (a> b) {
+        const temp = a;
+        a = b;
+        b = temp;
+      }
+    
     let sum = 0;
     for (let i = a; i <= b; i++){
         sum = sum + i;
