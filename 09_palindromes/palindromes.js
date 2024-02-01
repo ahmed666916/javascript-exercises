f87b777fb3e348ae3a513d5cad046a5ead62d0f9
ahmed666916 <ahmed666916@gmail.com>
2024-02-01 22:30:36 +0500

    first test passed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let reverse_string = '';
+    for (let i=string.length; i >= 0; i--){
+        reverse_string = reverse_string + string.charAt(i);
+        
+    }
+    if (string === reverse_string){
+        return true;
+    }
+    else{
+        return false;
+    }
+
 
 };
 
