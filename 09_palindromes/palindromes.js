abd4638f4603bc7c12625e4a375f8d540b98750a
ahmed666916 <ahmed666916@gmail.com>
2024-02-01 23:01:24 +0500

    code refactoring 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,16 +1,8 @@
 const palindromes = function (string) {
     let reverse_string = '';
     const cleanedStr = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    for (let i=cleanedStr.length; i >= 0; i--){
-        reverse_string = reverse_string + cleanedStr.charAt(i);
-        
-    }
-    if (cleanedStr === reverse_string){
-        return true;
-    }
-    else{
-        return false;
-    }
+    
+    return cleanedStr === cleanedStr.split('').reverse().join('');
 
 
 };
