c103671abbfc5ed21eb91f5423cffaf4e1805cb6
ahmed666916 <ahmed666916@gmail.com>
2024-02-01 21:51:28 +0500

    13 out of 16 tests passed for exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,24 +3,42 @@ const add = function(num1, num2) {
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 - num2;
 	
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let sum = 0;
+
+  numArray.forEach(element => {
+    sum += element;
+
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(num1, num2) {
+  return num1 * num2;
 
 };
 
-const power = function() {
+const power = function(num, power) {
+  
+  return num ** power;
+  }
 	
-};
 
-const factorial = function() {
-	
+
+const factorial = function(num) {
+  let fact = num;
+	for (let i = num - 1; i > 0; i--) {
+    fact = fact * i;
+    
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
