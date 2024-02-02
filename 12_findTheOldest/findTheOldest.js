4279d92c71dab9416db156213b242de3e9d2140c
ahmed666916 <ahmed666916@gmail.com>
2024-02-02 18:48:55 +0500

    finished writing the function to calculate the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
 const findTheOldest = function(people) {
-
-
+    let oldest = 0;
+    for (person in people) {
+        let yearBorn = people[person].yearOfBirth;
+        let yearDeath = people[person].yearOfDeath;
+        let age = findAge(yearBorn, yearDeath);
+        if (age > oldest) {
+            oldest = age;
+        }
+        
+    }
+    
+    return oldest;
 };
 
 function findAge(yearBorn, yearNow) {
