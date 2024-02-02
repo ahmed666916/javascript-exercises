bb9f7881881a61a60e6775446f5e46ccc70d026b
ahmed666916 <ahmed666916@gmail.com>
2024-02-02 18:55:22 +0500

    made the function to return name of the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,18 @@
 const findTheOldest = function(people) {
     let oldest = 0;
+    let name = '';
     for (person in people) {
         let yearBorn = people[person].yearOfBirth;
         let yearDeath = people[person].yearOfDeath;
         let age = findAge(yearBorn, yearDeath);
         if (age > oldest) {
             oldest = age;
+            name = people[person].name;
         }
         
     }
     
-    return oldest;
+    return name;
 };
 
 function findAge(yearBorn, yearNow) {
